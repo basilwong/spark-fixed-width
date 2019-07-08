36805a036264b23a295977ce0e65dda2c4d00ae5
basilwong <wong.bas@gmail.com>
2019-07-08 12:59:00 -0700

    passing two tests for lsplit

--- src/test/java/FlatFileParserTest.java
+++ src/test/java/FlatFileParserTest.java
@@ -31,17 +31,36 @@ public class FlatFileParserTest implements Serializable{
     }
 
     @Test
-    public void testSplit1() {
+    public void testlsplit1() {
         int[] rawArr = {0, 1, 2, 3};
         List<Integer> schema = Ints.asList(rawArr);
 
-        String testStr = "122333";
+        String testStr = "122333 garbage";
 
         Row result = SerUtil.lsplit(schema, testStr);
 
+        Assert.assertEquals("", result.getString(0));
         Assert.assertEquals("1", result.getString(1));
         Assert.assertEquals("22", result.getString(2));
         Assert.assertEquals("333", result.getString(3));
+    }
+
+    @Test
+    public void testlsplit2() {
+        int[] rawArr = {2, 1, 1, 1, 1, 1, 3};
+        List<Integer> schema = Ints.asList(rawArr);
+
+        String testStr = " ñüáéíóú ú";
+
+        Row result = SerUtil.lsplit(schema, testStr);
+
+        Assert.assertEquals("ñ", result.getString(0));
+        Assert.assertEquals("ü", result.getString(1));
+        Assert.assertEquals("á", result.getString(2));
+        Assert.assertEquals("é", result.getString(3));
+		Assert.assertEquals("í", result.getString(4));
+        Assert.assertEquals("ó", result.getString(5));
+		Assert.assertEquals("ú ú", result.getString(6));
     }
 
 }
