1a0b3a28bb5a4ad6b80b5b416233fb59e06950ab
basilwong <wong.bas@gmail.com>
2019-07-09 10:22:08 -0700

    Added skeleton for testFlatFileMakerTest

--- src/test/java/FlatFileMakerTest.java
+++ src/test/java/FlatFileMakerTest.java
@@ -1,2 +1,37 @@
-//public class FlatFileMakerTest {
-//}
+import org.apache.spark.SparkConf;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.util.*;
+import com.example.SerUtil;
+import com.example.FlatFileParser;
+import com.google.common.primitives.Ints;
+
+public class FlatFileMakerTest {
+    private static Dataset<Row> df;
+    private static SparkSession spark;
+
+    @BeforeClass
+    public static void beforeClass() {
+        spark = SparkSession.builder().master("local[*]").config(new SparkConf().set("fs.defaultFS", "file:///"))
+                .appName(FlatFileParserTest.class.getName()).getOrCreate();
+        df = spark.read().format("csv").option("header", "true").load("src/test/resources/credithistory.csv");
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        if (spark != null) {
+            spark.stop();
+        }
+    }
+
+    @Test
+    public void testMakerBasic() {
+    }
+}
+
